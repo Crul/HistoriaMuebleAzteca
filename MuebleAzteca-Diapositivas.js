37f17388092ee10c592ddca98a57022cee683c39
Raul J Vila <rauljavier.vila@gmail.com>
2025-01-16 03:40:44 +0100

    initial commit

---
+++ MuebleAzteca-Diapositivas.js
@@ -0,0 +1,51 @@
+let currentSlide = 0;
+let slides = [];
+let keyDown = false;
+function init() {
+    if (window.location.hash)
+        currentSlide = Number(window.location.hash.substr(1)) 
+
+    slides = [...document.querySelectorAll("body > div")];
+    slides[currentSlide].style.display = "block";
+    
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+
+    const imgs = [...document.querySelectorAll("img")];
+    for (let i in imgs)
+        imgs[i].onclick = handleImgClick;
+}
+
+function handleImgClick(x, y, z) {
+    window.open(x.currentTarget.src)
+}
+
+function handleKeyDown(ev) {
+    if (keyDown) return;
+    keyDown = true;
+    if (ev.key == "ArrowRight") {
+        slides[currentSlide].className = "outforward";
+        // slides[currentSlide].style.display = "none";
+        currentSlide++;
+        if (currentSlide >= slides.length)
+            currentSlide = 0;
+        slides[currentSlide].style.display = "block";
+        slides[currentSlide].className = "inforward";
+    } else if (ev.key == "ArrowLeft") {
+        //slides[currentSlide].style.display = "none";
+        slides[currentSlide].className = "outbackward";
+        currentSlide--;
+        if (currentSlide < 0)
+            currentSlide = slides.length - 1;
+        slides[currentSlide].style.display = "block";
+        slides[currentSlide].className = "inbackward";
+    }
+    window.location.hash = currentSlide;
+    // e.preventDefault();
+}
+
+function handleKeyUp() {
+    keyDown = false;
+}
+
+window.onload = init;
---
+++ README.md
@@ -0,0 +1,31 @@
+# Repositorio para el trabajo "Una muy breve introducción a la historia del mueble azteca" para la asignatura de Historia del Mueble de 1º de Ebanistería (EASS).
+
+Enlaces
+=======
+
+### Página con todos los enlaces:
+
+[https://crul.github.io/HistoriaMuebleAzteca](https://crul.github.io/HistoriaMuebleAzteca)
+
+### Trabajo (PDF)
+
+[https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca.pdf](https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca.pdf)
+
+### Trabajo (ODT)
+
+[https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca.odt](https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca.odt)
+
+### Diapositivas
+
+[https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca-Diapositivas.html](https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca-Diapositivas.html)
+
+### Diapositivas sin animaciones
+
+[https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca-Diapositivas-no-anim.html](https://crul.github.io/HistoriaMuebleAzteca/MuebleAzteca-Diapositivas-no-anim.html)
+
+---
+
+Licencias:
+- Texto: [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+- Código: [GNU AGPLv3](https://choosealicense.com/licenses/agpl-3.0/)
+- Imágenes: Consultar copyright en las fuentes
